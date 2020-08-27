61c9cb06ab4d9c975735f0071b21f604d759adb9
Deyvid William <deyvidwms@gmail.com>
2020-08-27 09:04:35 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import Tmdb from './Tmdb';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default () => {
+  
+  useEffect( ()=> {
+    const loadAll = async () => {
+      // pegando a lista TOTAL
+    }
+
+    loadAll();
+  }, [] );
+  
+  return(
+    <div>
+      Ola Mundo!
     </div>
   );
 }
-
-export default App;
