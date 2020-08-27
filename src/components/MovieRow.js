06850f7f6dd78e8e67dae6ffc31284d34e33ca24
Deyvid William <deyvidwms@gmail.com>
2020-08-27 09:30:10 -0300

    Transformando a MovieRow em Dinâmica

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './MovieRow.css';
 
 
-export default () => {
+export default ({title, items}) => {
     return (
         <div>
-            Lista do Filme
+            <h2>{title}</h2>
         </div>
     );
 }
