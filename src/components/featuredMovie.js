9ab409f54f36588b216e5fcdd7a70da6e2378953
Deyvid William <deyvidwms@gmail.com>
2020-08-27 11:16:44 -0300

    Exibindo as informações basicas do featuredMovie

--- src/components/featuredMovie.js
+++ src/components/featuredMovie.js
@@ -8,7 +8,16 @@ export default ({item}) => {
             backgroundPosition: 'center',
             backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
         }}>
-            <div>{item.original_name}</div>
+            <div className="featured--vertical">
+                <div className="featured--horizontal">
+                    <div className="featured--name">{item.original_name}</div>
+                    <div className="featured--info">
+                        <div className="featured--points">{item.vote_average} pontos</div>
+                        <div className="featured--year">2099</div>
+                        <div className="featured--seasons">{item.number_of_seasons} temporada{ item.number_of_seasons !== 1 ? 's' : '' }</div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
