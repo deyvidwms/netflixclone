6618dc224617140790a63e6689df95d3090ad06c
Deyvid William <deyvidwms@gmail.com>
2020-08-27 09:14:26 -0300

    Pegando e Guardando as informações da API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
 
 
 export default () => {
   
+  const [movieList, setMovieList] = useState([]);
+
   useEffect( ()=> {
     const loadAll = async () => {
       // pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
     }
 
     loadAll();
