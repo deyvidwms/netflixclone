8d8688cd02787cfd4d6bdc4ab47eaee908e4781b
Deyvid William <deyvidwms@gmail.com>
2020-08-27 09:39:33 -0300

    Exibindo capas dos filmes e series

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default () => {
     <div className="page">
       <section className="lists">
         {movieList.map( (item, key) => (
-          <MovieRow key={key} title={item.title} items={item.items} />
+          <MovieRow key={key} title={item.title} items={item.items} alt={item.original_title} />
         ) )}
       </section>
     </div>
