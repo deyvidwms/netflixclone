db6b46760be58ab715c7479dd485711db39fa254
Deyvid William <deyvidwms@gmail.com>
2021-09-28 15:48:02 -0300

    Atualização de imagens

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ export default ({black}) => {
         <header className={ black ? 'black' : '' } >
             <div className="header--logo">
                 <a href="/">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png" alt="Netflix" />
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" alt="Netflix" />
                 </a>
             </div>
             <div className="header--user">
--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -4,7 +4,7 @@ import './index.css';
 export default () => {
     return (
         <div className="loading">
-            <img src="https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/netflix-loading.gif" alt="Carregando" />
+            <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" />
         </div>
     );
 }
